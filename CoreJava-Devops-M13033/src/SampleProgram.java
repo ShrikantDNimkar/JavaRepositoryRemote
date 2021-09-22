e19732ddac8fae173d2565b627c08784c233d01b
Shrikant Nimkar <nimkar81@gmail.com>
2021-09-22 17:49:56 +0530

    Sample Programs of Java.

---
+++ CoreJava-Devops-M13033/src/SampleProgram.java
@@ -0,0 +1,32 @@
+
+public class SampleProgram {
+	
+	public static void main(String ars[]){
+		
+//		System.out.print("This is Sample Program.");
+//		System.out.println("THis is shortcut method.");
+//		System.out.print("This is Sample Program___.");
+		//syntax: data_type variablename =value;
+		byte floorNo=3;
+		short roomNo=705;
+		int eid=1020;
+		long mobileNumber=9876543210L;
+		
+		float rating =5.0f;
+		double salary=55000.00;
+		
+		char gender='M';
+		boolean maritlStatus=true;
+		
+		//display the result
+		System.out.println("Floor No: "+floorNo);
+		System.out.println("Room No: "+roomNo);
+		System.out.println("Employee ID: "+eid);
+		System.out.println("Mobile Number: "+mobileNumber);
+		System.out.println("Rating: "+rating);
+		System.out.println("Salary: "+salary);
+		System.out.println("Gender: "+gender);
+		System.out.println("Marital Status: "+maritlStatus);
+		
+	}
+}
