ee7e2aed72bffebb7af766d69035eb38e69dcb19
Shrikant Nimkar <nimkar81@gmail.com>
2021-09-23 13:32:14 +0530

    Looping in java.

--- CoreJava-Devops-M13033/src/com/hcl/java/fundamentals/LoopingStatements.java
+++ CoreJava-Devops-M13033/src/com/hcl/java/fundamentals/LoopingStatements.java
@@ -4,6 +4,9 @@ public class LoopingStatements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("----while loop----");
+		// initialize the value
 		int i=1;
 		while(i<=5)
 		{
@@ -11,7 +14,31 @@ public class LoopingStatements {
 			System.out.println("Value: "+i);
 			//increment/decrement
 			i++;
+		} 
+		
+		System.out.println();
+		System.out.println("----do while loop----");
+		// initialization
+		int x=1;
+		do {
+			System.out.println("Value: "+x);
+			x++;
+		}while(x<=5);
+		
+		System.out.println();
+		System.out.println("----for loop----");
+		for(int y=1;y<=5;y++) 
+		{
+			System.out.println("Value: "+y);
 		}
-	}
+		System.out.println("\n");
+		for(int y=1;y<=5;y++) 
+		{
+			System.out.println("Value: "+y);
+			if(y>2) 
+				continue;
+			System.out.println("This line will be printed when'continue' not excuted");
+		}
 
-}
+	}
+}	
