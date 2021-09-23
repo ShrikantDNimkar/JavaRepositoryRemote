2a176cb61bc9bdbf2e91611b9e854a8f1893e939
Shrikant Nimkar <nimkar81@gmail.com>
2021-09-23 12:59:37 +0530

    Looping(While)

--- CoreJava-Devops-M13033/src/com/hcl/java/fundamentals/DecisionMakingStatements.java
+++ CoreJava-Devops-M13033/src/com/hcl/java/fundamentals/DecisionMakingStatements.java
@@ -1,10 +1,51 @@
 package com.hcl.java.fundamentals;
 
-public class DecisionMakingStatements {
+import java.lang.System;
+ import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class DecisionMakingStatements 
+{
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("---------------------");
+		System.out.println("Hotel ABC");
+		System.out.println("---------------------");
+		
+		System.out.println("Enter your choice");
+		
+		// read the data from keyboard using scanner class
+		Scanner scan = new Scanner(System.in);
+		int number= scan.nextInt();
+		if(number>0)
+		{
+			switch (number) 
+			{
+				case 1:
+					System.out.println("Idly");
+					break;
+				case 2:
+					System.out.println("Wada");
+					break;	
+				case 3:
+					System.out.println("Puri");
+					break;	
+				case 4:
+					System.out.println("Dosa");
+					break;	
+				default:
+					System.out.println("No stock");
+			}		
+		}
+		else if(number == 0)
+		{
+			System.out.println("Please enter non zero values only");
+		}
+		else	
+		{
+			System.out.println("Please enter positive values only");
+		}
 	}
 
 }
---
+++ CoreJava-Devops-M13033/src/com/hcl/java/fundamentals/LoopingStatements.java
@@ -0,0 +1,17 @@
+package com.hcl.java.fundamentals;
+
+public class LoopingStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=5)
+		{
+			//result
+			System.out.println("Value: "+i);
+			//increment/decrement
+			i++;
+		}
+	}
+
+}
